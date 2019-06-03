ceda214975bb510006fcc93bf50bcf8552eda482
birna <briandam26@yahoo.com>
2019-06-03 15:53:13 -0700

    finished trivia logic with working timer and some styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,8 +34,8 @@ const questions = [
 
     {
         question: "Which of the following bomb defuse maps has sewers?",
-        choices: ["contra", "office", "dust", "inferno"],
-        correctChoice: 1
+        choices: ["office", "dust", "contra", "inferno"],
+        correctChoice: 2
     },
 
     {
@@ -52,32 +52,182 @@ const questions = [
 ];
 
 //timeout so there is time before something is displayed
-let ansTimeout = 2000;
+let ansTimeout = 2400;
 
 //score variables to keep track
+let questionCounter = 0;
 let correct = 0;
 let incorrect = 0;
 let missed = 0;
 
 //array for user's answers 
-let userAns = [];
+let userChoice = [];
 
 //question timer functions and variables
+let timeRem = 10;
+let increment;
+    
+function runTimer() {
+    increment = setInterval(decrement, 1000);
+};
+
+function decrement() {
+    timeRem--;
+    $("#time-rem").html("Time remaining: " + timeRem + " seconds");
+    if (timeRem === 0) {
+        stopTimer();
+        userChoice.length = 0;		
+        //record user answer 
+        let userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
+        userChoice.push(userSelection);
+        console.log(userChoice);
+        nextQ();
+    };
+};
+
+function resetTimer() {
+    timeRem = 10;
+    $("#time-rem").html("Time remaining: " + timeRem + " seconds");
+};
+
+function displayTimer() {
+    $("#time-rem").html("Review"+"</h3><br><h3>");
+};
+    
+function stopTimer() {
+    clearInterval(increment);
+};
+    
 
 //start game 
+function showStart() {
+    $("#content").append('<a href="#" class="btn btn-primary btn-lg" id="start-button">' + "Start" + '</a>');
+    $("#start-button").on("click", function(event) {
+        event.preventDefault();
+        //Reveals first question
+        firstQ();
+        resetTimer();
+    });
+};
+showStart();
 
 //show one question at a time
+function firstQ() {
+    let startContent = $("#content");
+    startContent.empty(); 
+    displayQ();
+};
 
-//if answer is correct, display screen congratulation for choosing right option then display next question after some time
+function displayQ() {
+    clearQ();
+    resetTimer();
+    $(".questions").html(questions[questionCounter].question);
+    //display response options
+    createRadios();
+    //submit button
+    $("#submit-div").append('<button type="submit" class="btn btn-default" id="submit">' + "Submit" + '</button>');
+    runTimer()
+    submitAns();
+};
 
 //taking in user's answers
+function submitAns() {
+    $("#submit").on("click", function(e) {
+        e.preventDefault();
+        userChoice.length = 0;
+            
+        //record user answer
+        let userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
+        userChoice.push(userSelection);
+        console.log(userChoice);
+        nextQ();
+    });
+};
+
+// display given options
+function createRadios() {
+    let responseOptions = $("#responses");
+    //empty array before user picks
+    responseOptions.empty();
+        
+    for (var i = 0; i < questions[questionCounter].choices.length; i++) {
+        responseOptions.append('<label><input type="radio" name="optionsRadios" id="optionsRadios2" value="' + [i] +'"><div class="cs-opt">' + questions[questionCounter].choices[i] + '</div></input><br></label>');
+    };
+};
 
 //changing questions
+function clearQ() {
+    let questionDiv = $(".questions");
+    questionDiv.empty();
+
+    let responsesDiv = $("#responses");
+    responsesDiv.empty();
+
+    let submitDiv = $("#submit-div");
+    submitDiv.empty();
+
+    let contentDiv = $("#content");
+    contentDiv.empty();
+
+    stopTimer();
+};
+
+function nextQ() {
+    checkQ();
+    questionCounter++;
+    //if the counter reaches the end of the array, show results and reset to 0
+    if (questionCounter === questions.length) {
+        setTimeout(showResults, ansTimeout);
+    } 
+    //otherwise display another question
+    else {
+        setTimeout(displayQ, ansTimeout);
+    };
+};
 
 //showing answer was correct or not
+function checkQ() {
+    clearQ();
+    let correctAnswer = questions[questionCounter].correctChoice;
+    if (userChoice[0] == questions[questionCounter].correctChoice) {
+        $("#content").append('<h3>'+"Good Job! You're 1337 AF" + '</h3>');
+        correct++;
+        displayTimer();
+    }
+    else if (userChoice[0] === undefined) {
+        $("#content").append('<h3>'+"Time's up!" + '</h3><br><h3>' + "The correct answer was: " + questions[questionCounter].choices[correctAnswer] + '</h3>');
+        missed++;
+        displayTimer();
+    }
+    else {
+        $("#content").append('<h3>'+"You noob, you chose wrong" + '</h3><br><h3>' + "The correct answer was: " + questions[questionCounter].choices[correctAnswer] + '</h3>');
+        incorrect++;
+        displayTimer();
+    };
+};
 
 //end game
+function showResults() {
+    clearQ();
+    $("#content").append('<h3>' + "Correct answers: " + correct + '</h3><br><h3>' + "Incorrect answers: " + incorrect + '</h3><br><h3>' + "Skipped questions: " + missed + '</h3><br><br><a href="#" class="btn btn-primary btn-lg" id="restart-button">' + "Restart" + '</a>');
+    //restart game
+    $("#restart-button").on("click", function(event) {
+        event.preventDefault();
+        //displays the first question
+        reset();
+        clearQ();
+        showStart();
+    });
+};
 
 //reset
+function reset() {
+    questionCounter = 0;
+    correct = 0;
+    incorrect = 0;
+    missed = 0;
+    userChoice = [];
+    resetTimer();
+};
 
 });
